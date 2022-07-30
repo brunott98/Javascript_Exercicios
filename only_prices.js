b4b5d093963280cf11eb8ea1342850bde692ad00
Bruno <104589217+brunott98@users.noreply.github.com>
2022-07-30 16:56:41 -0300

    Metodologia

--- only_prices.js
+++ only_prices.js
@@ -1,3 +1,13 @@
+
+Array.prototype.map2 = function(callback){
+   const newArray = []
+   for (let i = 0; i < this.length; i++) {
+        newArray.push(callback(this[i],i,this))
+   }
+   return newArray
+}
+
+
 const purchase = ['{"name":"Pen","price":3.45}',
 '{"name":"Book","price":13.90}',
 '{"name":"Kit","price":41.22}',
@@ -5,12 +15,24 @@ const purchase = ['{"name":"Pen","price":3.45}',
 
 //Return a array with only the prices
 
+/*
 const newpurchase = purchase.map( i => JSON.parse(i))
 console.log(newpurchase)
 
-const values_1 =  newpurchase.map( i => Object.entries(i).map(x => x[1]))
+const values_1 =  newpurchase.map( j => Object.entries(j)).map(x => x[1])
 console.log(values_1)
 
-const values_2 = values_1.map(z => z[1])
-console.log(values_2) 
+const getValues = values_1.map(z => z[1])
+console.log(getValues) 
+
+
+ */
+
+
+const purchaseObject = purchase.map( i => JSON.parse(i))
+console.log(purchaseObject)
+
+const prices = purchaseObject.map2(product => {return product.price}) 
+console.log(prices)
+
 
